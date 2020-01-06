7628bf4849cd2805ebc8d1288428095602242e68
chenzeyu <624205157@qq.com>
2020-01-06 14:32:37 +0800

    202016PAD待办

--- app/src/main/java/com/hulian/oa/net/Urls.java
+++ app/src/main/java/com/hulian/oa/net/Urls.java
@@ -4,6 +4,6 @@ package com.hulian.oa.net;
 
    public static String commUrls= "http://47.92.151.250:8081/";
 //   public static String commUrls= "http://192.168.1.101:80/";
-//   public static String commUrls= "http://192.168.0.134:80/";
+//   public static String commUrls= "http://192.168.0.134:80/".;
 
 }
