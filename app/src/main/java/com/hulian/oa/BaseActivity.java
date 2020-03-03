bc10a62a7bf66fbd6ff1213568edeebd29a78b0a
xingqinggele <3297289196@qq.com>
2020-03-03 16:47:39 +0800

    消息通知更换头像

--- app/src/main/java/com/hulian/oa/BaseActivity.java
+++ app/src/main/java/com/hulian/oa/BaseActivity.java
@@ -38,6 +38,7 @@ public class BaseActivity extends FragmentActivity  {
     public FragmentManager fm;
     public FragmentTransaction ft;
     public static List<Activity> activitys;
+
     //das
     /**加载对话框*/
     private Dialog dialogLoading;
