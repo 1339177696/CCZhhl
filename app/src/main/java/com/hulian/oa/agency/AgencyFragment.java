10dcb4aa776407a29e57b911cdfcda0ff7d38ef3
chenzeyu <624205157@qq.com>
2020-03-17 09:38:18 +0800

    待办问题解决

--- app/src/main/java/com/hulian/oa/agency/AgencyFragment.java
+++ app/src/main/java/com/hulian/oa/agency/AgencyFragment.java
@@ -40,6 +40,7 @@ public class AgencyFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
 //    @BindView(R.id.my_tablayout)
 //    TabLayout myTablayout;
 //    @BindView(R.id.my_viewpager)
