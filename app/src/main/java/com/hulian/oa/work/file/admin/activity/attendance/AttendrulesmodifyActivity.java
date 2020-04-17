c3df6fe8d0f1635e0c0a9c255d16db39039a8d2e
青格乐 <3297289196@qq.com>
2020-04-17 08:50:50 +0800

    打卡日历2

---
+++ app/src/main/java/com/hulian/oa/work/file/admin/activity/attendance/AttendrulesmodifyActivity.java
@@ -0,0 +1,21 @@
+package com.hulian.oa.work.file.admin.activity.attendance;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+
+import com.hulian.oa.BaseActivity;
+
+/**
+ * 作者：qgl 时间： 2020/4/16 17:10
+ * Describe:
+ */
+public class AttendrulesmodifyActivity extends BaseActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
